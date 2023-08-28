2cdc638d122075b690071eef6995d1bb53571d74
RoBaertschi <rtmbaertschi007@gmail.com>
2023-08-28 11:04:50 +0200

    ussser

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -3,9 +3,11 @@
 export default class User {
   username: string
   password: string
+  creationDate: bigint
 
-  constructor(username:string, password:string){
+  constructor(username:string, password:string, creationDate: bigint){
     this.username = username
     this.password = password
+    this.creationDate = creationDate
   };
 }
