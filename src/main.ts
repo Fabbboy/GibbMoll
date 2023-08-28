c532d32270e45701392f9e450649fceed9c79766
Fabrice Schaub <fabrice.schaub@swisscom.com>
2023-08-28 09:55:52 +0200

    bug fix with file folder

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 async function bootstrap() {
-  fs.mkdirSync(".cloud");
+  if(!fs.existsSync(".cloud")) 
+    fs.mkdirSync(".cloud");
+  
 
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
