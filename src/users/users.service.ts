3ba638946a89524cb5b7aa5e5839e85ec937d01e
RoBaertschi <rtmbaertschi007@gmail.com>
2023-08-28 11:06:48 +0200

    ssss

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import database from "src/Database/Database";
+import User from "src/entities/User";
 
 @Injectable()
 export class UsersService {
   async findOne(username: string) {
-    let result = database.run("SELECT * from users WHERE username = ?", username)
+    let result = await database.run("SELECT * from users WHERE username = ?", username)
     console.log(result)
-    return result
+
+
+    return new User(result.username, result.password, result.creationDate)
   }
 }
