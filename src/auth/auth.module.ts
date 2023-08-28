df36e3054e22e6688d7cd61b378269a0d46adb4f
RoBaertschi <rtmbaertschi007@gmail.com>
2023-08-28 10:59:53 +0200

    shsh

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import AuthController from "./auth.controller";
 import AuthService from "./auth.service";
-import { UsersService } from "../users/users.service";
+import { UsersModule } from "src/users/users.modules";
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController], 
-  providers: [AuthService, UsersService],     
+  providers: [AuthService],     
 })
 export default class AuthModule {}
