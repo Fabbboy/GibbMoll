3091161f8e0c69aadb4fc104d39d29062a4fca3f
RoBaertschi <rtmbaertschi007@gmail.com>
2023-09-04 10:37:14 +0200

    welp

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import User from 'src/entities/User';
+import { LocalAuthGuard } from './local-guard.guard';
 
 @Controller('auth')
 export class AuthController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return req.user;
+    let user: User = req.user;
+
+    return {id: user.id, 
+            username: user.username,
+          password: user.password,
+        creationDate: user.creationDate.toString()};
   }
 }
