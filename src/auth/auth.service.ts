451ee85ac9d84c2312f6d70eede672fcb83e6375
RoBaertschi <rtmbaertschi007@gmail.com>
2023-09-04 11:20:36 +0200

    format

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,22 +5,28 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService, private jwtService: JwtService) {}
-  async validateUser(username: string, pass: string): Promise<UserWithoutPassword | null> {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+  async validateUser(
+    username: string,
+    pass: string,
+  ): Promise<UserWithoutPassword | null> {
     const user = await this.usersService.findOne(username);
-    if(user && user.password == pass) {
-      console.log(user)
-      const {password, ...result} = user;
-      console.log(result)
+    if (user && user.password == pass) {
+      console.log(user);
+      const { password, ...result } = user;
+      console.log(result);
       return result;
     }
     return null;
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.id }
+    const payload = { username: user.username, sub: user.id };
     return {
-      access_token: this.jwtService.sign(payload)
+      access_token: this.jwtService.sign(payload),
     };
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,12 @@ import User from 'src/entities/User';
 @Injectable()
 export class UsersService {
   async findOne(username: string) {
-    let result: User = (await database.connector.query('SELECT * from users WHERE username = ?', username) as Array<User>).pop()
+    let result: User = (
+      (await database.connector.query(
+        'SELECT * from users WHERE username = ?',
+        username,
+      )) as Array<User>
+    ).pop();
     console.log(result);
     return new User(
       result.username,
