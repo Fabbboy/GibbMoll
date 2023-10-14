b3a6f33e25c2d203533e99ae5cd62e855b36c0d4
RoBaertschi <rtmbaertschi007@gmail.com>
2023-10-14 22:41:31 +0200

    Added ability to update the admin password

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,6 +3,7 @@ import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { DatabaseService } from 'src/database/database.service';
+import { User } from '@prisma/client';
 
 @Module({
   providers: [AdminService],
@@ -14,18 +15,27 @@ export class AdminModule implements OnApplicationBootstrap {
   constructor(private databaseService: DatabaseService) {}
   async onApplicationBootstrap() {
     try {
-      this.databaseService.user.findFirstOrThrow({
+      const admin: User = await this.databaseService.user.findFirstOrThrow({
         where: { username: 'admin' },
       });
-    } catch (e) {}
-    console.log('Adding User Admin');
-     await this.databaseService.user.create({
-      data: {
-        isAdmin: true,
-        creationDate: Date.now(),
-        password: process.env['ADMIN_PASSWORD'],
-        username: 'admin',
-      },
-    });
+
+      if (admin.password !== process.env['ADMIN_PASSWORD']) {
+        console.log('Updating Admin Password');
+        await this.databaseService.user.update({
+          where: admin,
+          data: { password: process.env['ADMIN_PASSWORD'] },
+        });
+      }
+    } catch (e) {
+      console.log('Adding User Admin');
+      await this.databaseService.user.create({
+        data: {
+          isAdmin: true,
+          creationDate: Date.now(),
+          password: process.env['ADMIN_PASSWORD'],
+          username: 'admin',
+        },
+      });
+    }
   }
 }
