1d49e542759f6f3f8020ba38d7cb633ddbb5dc2d
RoBaertschi <rtmbaertschi007@gmail.com>
2023-10-28 19:24:59 +0200

    added is route to admin to check if you are an admin

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { AdminService } from './admin.service';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  constructor(private adminService: AdminService) {}
+
+  @Get('is')
+  async isAdmin(@Req() req) {
+    const username = req.user.username;
+    return { isAdmin: await this.adminService.isUserAdmin(username) };
+  }
+}
